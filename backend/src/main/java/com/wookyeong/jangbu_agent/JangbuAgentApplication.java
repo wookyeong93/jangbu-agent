c886c30fc1e5083c8821455e131af6f10395d01d
wooykeong93 <dnrud368@gmail.com>
2026-06-09 10:39:26 +0900

    Spring Boot 프로젝트 생성 및 README 초안 작성

---
+++ backend/src/main/java/com/wookyeong/jangbu_agent/JangbuAgentApplication.java
@@ -0,0 +1,13 @@
+package com.wookyeong.jangbu_agent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JangbuAgentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JangbuAgentApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/wookyeong/jangbu_agent/JangbuAgentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wookyeong.jangbu_agent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JangbuAgentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
